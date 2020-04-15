f405ad7f5acefd4ab4392e508c05e746b8d37508
dwony26 <arbana@naver.com>
2020-04-15 20:46:42 +0900

    Pass arguments to a named route

---
+++ lib/scaffold.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: 'Flutter Tutorial',
+    home: TutorialHome(),
+  ));
+}
+
+class TutorialHome extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          tooltip: "Navigation menu",
+          onPressed: null,
+        ),
+
+        title: Text("Example Title"),
+
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            tooltip: "Search",
+            onPressed: null,
+          )
+        ],
+      ),
+
+      body: Center(
+        child: Text("Hello World!"),
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        tooltip: "Add",
+        child: Icon(Icons.add),
+        onPressed: null,
+      ),
+    );
+  }
+}
